baecd4cfb640348c387520f08070efaf8b4357af
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-09-29 06:25:09 +0600

    fakeData.json set

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,14 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcaseClock } from '@fortawesome/free-solid-svg-icons'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Players.css'
 
 const Players = () => {
+    const [players, setPlayers] = useState([]);
+    useEffect(() => {
+        fetch('fakeData.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, []);
     return (<div className='activities'>
         <div className="players-container">
             <div className='fontAwsome' >
                 <FontAwesomeIcon icon={faBriefcaseClock}></FontAwesomeIcon>
                 <h2>Cricket-Club</h2>
+
+            </div>
+            <div className='short-p'>
+                <h3>Select Best Players</h3>
             </div>
         </div>
         <div className="cart-container">
